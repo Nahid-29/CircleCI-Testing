6eabf1ba6d806c7ebb07ce478213ffbb4acc6b0d
Nahid-29 <kamrulhasannahid2002@gmail.com>
2024-10-25 00:01:56 +0600

    _

--- test_sum.py
+++ test_sum.py
@@ -3,7 +3,7 @@ from sum import summation  # Replace 'your_module' with the actual module name
 
 def test_summation():
     # Test case 1: Check summation of a list of positive numbers
-    assert summation([1, 2, 3, 4, 5]) == 16
+    assert summation([1, 2, 3, 4, 5]) == 15
     
     # Test case 2: Check summation with negative numbers
     assert summation([-1, -2, -3, -4, -5]) == -15
